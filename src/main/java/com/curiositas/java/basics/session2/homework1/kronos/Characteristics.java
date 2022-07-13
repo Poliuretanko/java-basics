72b2ded90fef0a616b073ba6ad5e14ab5fcce739
Konstantin Vesninskii <kvesninskii@plesk.com>
2022-07-13 23:01:53 +0700

    Finally!

---
+++ src/main/java/com/curiositas/java/basics/session2/homework1/kronos/Characteristics.java
@@ -0,0 +1,6 @@
+package com.curiositas.java.basics.session2.homework1.kronos;
+
+public interface Characteristics {
+    String strongness();
+    boolean isWinner(boolean result);
+}
---
+++ src/main/java/com/curiositas/java/basics/session2/homework1/kronos/Scissors.java
@@ -0,0 +1,14 @@
+package com.curiositas.java.basics.session2.homework1.kronos;
+
+public class Scissors extends FigureType{
+    @Override
+    public String name() {return "Scissors";}
+    @Override
+    public String strongness() {
+        return name() + " is stronger!!";
+    }
+    @Override
+    public boolean isWinner(boolean result){
+        return result;
+    }
+}
