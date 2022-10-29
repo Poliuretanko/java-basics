ecbefa182c35180515114d3989a7122b48e7461c
dgorbatykh <dgorbatykh@plesk.com>
2022-10-29 22:33:04 +0200

    Prepare 9th session

--- src/main/java/com/curiositas/java/basics/session9/examples/Step37_LeakingMemory.java
+++ src/main/java/com/curiositas/java/basics/session9/examples/Step37_LeakingMemory.java
@@ -2,7 +2,7 @@ package com.curiositas.java.basics.session9.examples;
 
 import com.curiositas.java.basics.session9.examples.tasks.LeakingMemoryTask;
 
-public class Step36_LeakingMemory {
+public class Step37_LeakingMemory {
     public static void main(String[] args) {
         var task = new LeakingMemoryTask();
         var application = new Application(task, 3);
--- src/main/java/com/curiositas/java/basics/session9/examples/Step38_OverloadingCPU.java
+++ src/main/java/com/curiositas/java/basics/session9/examples/Step38_OverloadingCPU.java
@@ -2,7 +2,7 @@ package com.curiositas.java.basics.session9.examples;
 
 import com.curiositas.java.basics.session9.examples.tasks.SearchingPrimeNumberTask;
 
-public class Step37_OverloadingCPU {
+public class Step38_OverloadingCPU {
     public static void main(String[] args) {
         var task = new SearchingPrimeNumberTask();
         var application = new Application(task, 15);
