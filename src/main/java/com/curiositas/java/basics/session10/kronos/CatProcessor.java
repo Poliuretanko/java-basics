023cd43a4047a27e2fe23938d246d78c67b155a3
Konstantin Vesninskii <kvesninskii@plesk.com>
2022-11-13 09:08:52 +0200

    Konstantin homework #10 Work with DB

---
+++ src/main/java/com/curiositas/java/basics/session10/kronos/CatProcessor.java
@@ -0,0 +1,47 @@
+package com.curiositas.java.basics.session10.kronos;
+
+import com.curiositas.java.basics.session10.kronos.dao.CatDao;
+import com.curiositas.java.basics.session10.kronos.model.Cat;
+import com.github.javafaker.Faker;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
+
+@SpringBootApplication
+public class CatProcessor {
+
+    private final static Faker faker = new Faker();
+    private final static Random random = new Random();
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatProcessor.class, args);}
+
+    @Bean
+    public CommandLineRunner demo(CatDao catDao){
+            return (args) -> {
+              var cat = generateNewCat();
+              var savedCat = catDao.save(cat);
+              /*catDao.upsert(savedCat);
+
+               */
+              var cats = catDao.findAll();
+              System.out.println(cats);
+            };
+    }
+
+    @NotNull
+    private static Cat generateNewCat() {
+        var nickname = faker.name().firstName();
+        var breed = faker.cat().breed(); //~70 years
+        var age = random.nextLong(18);
+        Cat cat = new Cat();
+        cat.setNickname(nickname);
+        cat.setBreed(breed);
+        cat.setAge(age);
+        return cat;
+    }
+}
