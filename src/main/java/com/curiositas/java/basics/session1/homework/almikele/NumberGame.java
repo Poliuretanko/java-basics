1cdd3ad51ccdf5ce4b46ffab9399ea89eb94dba3
almikele <cheldyshkina@gmail.com>
2022-12-12 20:04:14 +0200

    almikele: number game for lesson 1 - add some updates

--- src/main/java/com/curiositas/java/basics/session1/homework/almikele/NumberGame.java
+++ src/main/java/com/curiositas/java/basics/session1/homework/almikele/NumberGame.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
  * 4. Give user info about right way
  */
 public class NumberGame {
+
+    public static final String INTEGER_NUMBER_MESSAGE = "Wrong! You should input integer number!";
+
     public static void main(String[] args) {
         int lowInterval = 0;
         int highInterval = 10;
@@ -17,9 +20,17 @@ public class NumberGame {
         int triesCounter;
         Scanner scanner = new Scanner(System.in);
 
-        if (args.length == 2) {
-            highInterval = Integer.parseInt(args[0]);
-            numberOfTries = Integer.parseInt(args[1]);
+        try {
+            if (args.length == 2) {
+                highInterval = Integer.parseInt(args[0]);
+                numberOfTries = Integer.parseInt(args[1]);
+            }
+        } catch (Exception e) {
+            System.out.println(INTEGER_NUMBER_MESSAGE);
+        }
+
+        if (lowInterval > highInterval) {
+            System.out.println("Wrong! Higher interval should more than lower interval");
         }
 
         System.out.printf("""
@@ -32,22 +43,28 @@ public class NumberGame {
         for (triesCounter = 1; triesCounter <= numberOfTries; triesCounter++) {
             System.out.print("\nYour guess is: ");
             String inputLine = scanner.nextLine();
-            int usersChoice = Integer.parseInt(inputLine);
-            if (usersChoice < lowInterval || usersChoice > highInterval) {
-                checkUsersWrongNumber(
-                        String.format("You should choose from %s to %s!", lowInterval, highInterval),
-                        numberOfTries,
-                        triesCounter);
-            } else if (usersChoice < randomIntFromComputer) {
-                checkUsersWrongNumber("It's more than yours!", numberOfTries, triesCounter);
-            } else if (usersChoice > randomIntFromComputer) {
-                checkUsersWrongNumber("It's less than yours!", numberOfTries, triesCounter);
-            } else {
-                System.out.println("Bingo! You won!\nThank you for game! See ya next time!");
-                break;
+            try {
+                int usersChoice = Integer.parseInt(inputLine);
+
+                if (usersChoice < lowInterval || usersChoice > highInterval) {
+                    checkUsersWrongNumber(
+                            String.format("You should choose from %s to %s!", lowInterval, highInterval),
+                            numberOfTries,
+                            triesCounter);
+                } else if (usersChoice < randomIntFromComputer) {
+                    checkUsersWrongNumber("It's more than yours!", numberOfTries, triesCounter);
+                } else if (usersChoice > randomIntFromComputer) {
+                    checkUsersWrongNumber("It's less than yours!", numberOfTries, triesCounter);
+                } else {
+                    System.out.println("Bingo! You won!\nThank you for game! See ya next time!");
+                    break;
+                }
+            } catch (Exception e) {
+                checkUsersWrongNumber(INTEGER_NUMBER_MESSAGE, numberOfTries, triesCounter);
             }
         }
-        if (triesCounter == 6) {
+
+        if (triesCounter > numberOfTries) {
             System.out.println("\nSorry, you loose... Try again next time!");
         }
     }
