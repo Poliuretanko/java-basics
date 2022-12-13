0ddf7f0a25cdfd5e5b4576e52eb3fa93cfb0da95
KosachevAA <Vartlokkyr@gmail.com>
2022-12-14 06:28:14 +0700

    Fix

--- src/main/java/com/curiositas/java/basics/session1/homework/KosachevAA/HomeW_1.java
+++ src/main/java/com/curiositas/java/basics/session1/homework/KosachevAA/HomeW_1.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class HomeW_1 {
     public static void main(String[] args) {
 
+        System.out.println("Rules of the game. You can only enter numbers from 1 to 10. To interrupt the game, enter enough.\nLet's play a game!");
+
         final int VictoryGameNumber = (int)(Math.random()*11);
         boolean EndGame = false;
         Scanner scanner = new Scanner(System.in);
