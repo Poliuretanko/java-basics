21f8b6bc6a781387fe6ae1506def1de29836126e
dgorbatykh <dgorbatykh@plesk.com>
2023-01-19 12:16:08 +0100

    Cleaning code

--- src/main/java/com/curiositas/java/basics/session3/examples/composition/Bread.java
+++ src/main/java/com/curiositas/java/basics/session3/examples/composition/Bread.java
@@ -1,21 +1,7 @@
 package com.curiositas.java.basics.session3.examples.composition;
 
 public class Bread {
-    private String type;
-
-    public Bread(String type) {
-        this.type = type;
-    }
-
-    public Bread() {
-        this.type = "white";
-    }
-
     String getDescription() {
         return "Bread";
-    }
-
-    public Bread copy() {
-        return new Bread(type);
     }
 }
--- src/main/java/com/curiositas/java/basics/session3/examples/homework/kronos/Clothes.java
+++
@@ -1,7 +0,0 @@
-package com.curiositas.java.basics.session3.examples.homework.kronos;
-
-public interface Clothes {
-    String Dressed(boolean isDressed);
-    String Utilized(boolean isUtilized);
-    String Cleaned(boolean isCleaned);
-}
