8eac19c1a2458725a1bd17a0fc3fb5bb94414501
KosachevAA <Vartlokkyr@gmail.com>
2023-01-29 11:48:38 +0700

    Homework 3

--- src/main/java/com/curiositas/java/basics/session3/examples/homework/kosachev/Shooter.java
+++ src/main/java/com/curiositas/java/basics/session3/examples/homework/kosachev/Shooter.java
@@ -5,19 +5,15 @@ public class Shooter {
     public static void main(String[] args) {
         Magazine recharge = new Magazine(3);
         Rifle rifle = new Rifle(recharge);
-
-
-        HeavyBullet bullet1 = new HeavyBullet();
-        HeavyBullet bullet2 = new HeavyBullet();
-        HeavyBullet bullet3 = new HeavyBullet();
-
-
         var retrieveMagazine = rifle.retrieveMagazine();
 
-        retrieveMagazine.putBullet(bullet1);
-        retrieveMagazine.putBullet(bullet2);
-        retrieveMagazine.putBullet(bullet3);
+        System.out.println("Is magazine charged: "+retrieveMagazine.isMagazineCharged());
 
+        while (!retrieveMagazine.isMagazineCharged()){ //Каждый раз будет инициализироваться новая пуля класса HeavyBullet
+            HeavyBullet bullet = new HeavyBullet();
+            System.out.println(bullet.insertBullet());
+            retrieveMagazine.putBullet(bullet);
+        }
 
         System.out.println("Is magazine charged: "+retrieveMagazine.isMagazineCharged());
         System.out.println("Number of bullets in magazine: "+retrieveMagazine.getNumberOfBullets());
